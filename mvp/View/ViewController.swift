45a627bb7990de1c645d6aedfcda397eadb5f12e
Bokhulenkov <bokhulenkov@gmail.com>
2024-12-19 10:23:18 +0300

    add gitignore

---
+++ mvp/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mvp
+//
+//  Created by Alexander Bokhulenkov on 19.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
